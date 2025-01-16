6f739c6afec616e55acad8752e8ef9da6f2241b9
Ntholi Nkhatho <ntholi@live.com>
2025-01-16 13:31:28 +0200

    create home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,16 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className='flex flex-col items-center px-4'>
+      <div className='mt-32 w-full sm:w-1/2 text-center flex flex-col items-center gap-10'>
+        <h1 className='border p-2.5 border-foreground/30 rounded text-xs sm:text-sm bg-foreground/5'>
+          Apply to Study at Limkokwing University Lesotho
+        </h1>
+        <p className='text-5xl sm:text-7xl md:text-8xl font-bold uppercase'>
+          Be The Most Successful
+        </p>
+        <Button className='sm:mt-8 w-48 rounded-full'>Apply Now</Button>
+      </div>
+    </main>
   );
 }
