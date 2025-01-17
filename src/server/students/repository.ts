df940d6a959ecc3723078cc7fe7d9e355591f996
Ntholi Nkhatho <ntholi@live.com>
2025-01-17 21:46:49 +0200

    when getting a single student, get student with application an qualification

--- src/server/students/repository.ts
+++ src/server/students/repository.ts
@@ -10,10 +10,24 @@ export default class StudentRepository extends BaseRepository<
     super(students, 'id');
   }
 
+  override async findById(id: number) {
+    return await db.query.students.findFirst({
+      where: (students, { eq }) => eq(students.id, id),
+      with: {
+        applications: true,
+        qualifications: true,
+      },
+    });
+  }
+
   async findByUserId(userId: string | undefined) {
     if (!userId) return undefined;
     return await db.query.students.findFirst({
       where: (students, { eq }) => eq(students.userId, userId),
+      with: {
+        applications: true,
+        qualifications: true,
+      },
     });
   }
 }
