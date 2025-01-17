3aebf760409fd4bbf14929c524c197de5f972700
Ntholi Nkhatho <ntholi@live.com>
2025-01-17 12:14:17 +0200

    create dedicated loading page

--- src/app/(home)/(auth)/apply/page.tsx
+++ src/app/(home)/(auth)/apply/page.tsx
@@ -1,17 +1,7 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
-import { Loader2 } from 'lucide-react';
-import { Suspense } from 'react';
 
 export default async function ApplyPage() {
-  return (
-    <Suspense fallback={<LoadingState />}>
-      <SessionComponent />
-    </Suspense>
-  );
-}
-
-async function SessionComponent() {
   const session = await auth();
   if (!session) {
     return (
@@ -25,13 +15,4 @@ async function SessionComponent() {
   }
 
   return redirect('/apply/student-details');
-}
-
-function LoadingState() {
-  return (
-    <div className='flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center p-4'>
-      <Loader2 className='h-6 w-6 animate-spin text-primary' />
-      <p className='mt-2 text-sm text-muted-foreground'>Loading...</p>
-    </div>
-  );
 }
