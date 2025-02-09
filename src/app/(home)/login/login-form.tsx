5a9bc1b3cb0246febe912a8ef7aea9986d91ac79
Ntholi Nkhatho <ntholi@live.com>
2025-02-09 19:25:12 +0200

    remove option for login with Facebook

--- src/app/(home)/login/login-form.tsx
+++ src/app/(home)/login/login-form.tsx
@@ -9,15 +9,14 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { BsFacebook } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
-import { facebookLogin, googleLogin } from './actions';
+import { googleLogin } from './actions';
 
 export default function LoginForm() {
   return (
     <Card className='w-full max-w-md p-6'>
       <CardHeader className='space-y-1'>
-        <CardTitle className='text-3xl text-center'>Login</CardTitle>
+        <CardTitle className='text-center text-3xl'>Login</CardTitle>
         <CardDescription className='text-center'>
           Sign in to access your account
         </CardDescription>
@@ -25,23 +24,15 @@ export default function LoginForm() {
       <CardContent className='space-y-4'>
         <Button
           onClick={googleLogin}
-          variant='outline'
-          className='w-full flex items-center justify-center space-x-2'
+          className='flex w-full items-center justify-center space-x-2'
         >
           <FcGoogle className='size-5' />
           <span>Continue with Google</span>
         </Button>
-        <Button
-          onClick={facebookLogin}
-          className='w-full flex items-center justify-center space-x-2'
-        >
-          <BsFacebook className='size-5 text-[#1877F2]' />
-          <span>Continue with Facebook</span>
-        </Button>
       </CardContent>
       <CardFooter className='flex flex-col space-y-2'>
-        <div className='text-sm text-center'>
-          By continuing, you agree to our Terms of Service and Privacy Policy
+        <div className='text-center text-sm'>
+          Your application progress will be associated with this account.
         </div>
       </CardFooter>
     </Card>
