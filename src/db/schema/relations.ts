a10bbb5158239d08c46abbf01d2d65740ed1dcde
Ntholi Nkhatho <ntholi@live.com>
2025-02-09 22:28:59 +0200

    refactor: update relations in database schema to enhance user and application associations

--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -1,39 +1,124 @@
 import { relations } from 'drizzle-orm';
-import { users } from './auth';
 import {
   applications,
   documents,
+  programChoices,
   programQualifications,
   programs,
   qualificationGrades,
   qualifications,
+  requiredSubjects,
   studentInfo,
   studentQualifications,
   studentSubjects,
   subjects,
-} from './index';
+} from './';
+import { users } from './auth';
 
-export const usersRelations = relations(users, ({ many, one }) => ({
-  studentInfo: one(studentInfo, {
-    fields: [users.id],
-    references: [studentInfo.userId],
-  }),
+export const usersRelations = relations(users, ({ many }) => ({
   applications: many(applications),
-  studentQualifications: many(studentQualifications),
-  documents: many(documents),
 }));
 
-export const applicationsRelations = relations(applications, ({ one }) => ({
-  user: one(users, {
-    fields: [applications.userId],
-    references: [users.id],
+export const qualificationsRelations = relations(
+  qualifications,
+  ({ many }) => ({
+    subjects: many(subjects),
+    grades: many(qualificationGrades),
+    programQualifications: many(programQualifications),
+    studentQualifications: many(studentQualifications),
   }),
-  firstChoice: one(programs, {
-    fields: [applications.firstChoiceId],
-    references: [programs.id],
+);
+
+export const subjectsRelations = relations(subjects, ({ one, many }) => ({
+  qualification: one(qualifications, {
+    fields: [subjects.qualificationId],
+    references: [qualifications.id],
   }),
-  secondChoice: one(programs, {
-    fields: [applications.secondChoiceId],
+  requiredSubjects: many(requiredSubjects),
+  studentSubjects: many(studentSubjects),
+}));
+
+export const qualificationGradesRelations = relations(
+  qualificationGrades,
+  ({ one, many }) => ({
+    qualification: one(qualifications, {
+      fields: [qualificationGrades.qualificationId],
+      references: [qualifications.id],
+    }),
+    requiredSubjects: many(requiredSubjects),
+    studentSubjects: many(studentSubjects),
+  }),
+);
+
+export const programsRelations = relations(programs, ({ many }) => ({
+  programQualifications: many(programQualifications),
+  programChoices: many(programChoices),
+}));
+
+export const programQualificationsRelations = relations(
+  programQualifications,
+  ({ one, many }) => ({
+    program: one(programs, {
+      fields: [programQualifications.programId],
+      references: [programs.id],
+    }),
+    qualification: one(qualifications, {
+      fields: [programQualifications.qualificationId],
+      references: [qualifications.id],
+    }),
+    requiredSubjects: many(requiredSubjects),
+  }),
+);
+
+export const requiredSubjectsRelations = relations(
+  requiredSubjects,
+  ({ one }) => ({
+    programQualification: one(programQualifications, {
+      fields: [requiredSubjects.programId, requiredSubjects.qualificationId],
+      references: [
+        programQualifications.programId,
+        programQualifications.qualificationId,
+      ],
+    }),
+    subject: one(subjects, {
+      fields: [requiredSubjects.subjectId],
+      references: [subjects.id],
+    }),
+    grade: one(qualificationGrades, {
+      fields: [requiredSubjects.gradeId],
+      references: [qualificationGrades.id],
+    }),
+  }),
+);
+
+export const applicationsRelations = relations(
+  applications,
+  ({ one, many }) => ({
+    user: one(users, {
+      fields: [applications.userId],
+      references: [users.id],
+    }),
+    studentInfo: one(studentInfo),
+    programChoices: many(programChoices),
+    studentQualifications: many(studentQualifications),
+    documents: many(documents),
+  }),
+);
+
+export const studentInfoRelations = relations(studentInfo, ({ one }) => ({
+  application: one(applications, {
+    fields: [studentInfo.applicationId],
+    references: [applications.id],
+  }),
+}));
+
+export const programChoicesRelations = relations(programChoices, ({ one }) => ({
+  application: one(applications, {
+    fields: [programChoices.applicationId],
+    references: [applications.id],
+  }),
+  program: one(programs, {
+    fields: [programChoices.programId],
     references: [programs.id],
   }),
 }));
@@ -41,9 +126,9 @@ export const applicationsRelations = relations(applications, ({ one }) => ({
 export const studentQualificationsRelations = relations(
   studentQualifications,
   ({ one, many }) => ({
-    user: one(users, {
-      fields: [studentQualifications.userId],
-      references: [users.id],
+    application: one(applications, {
+      fields: [studentQualifications.applicationId],
+      references: [applications.id],
     }),
     qualification: one(qualifications, {
       fields: [studentQualifications.qualificationId],
@@ -71,30 +156,9 @@ export const studentSubjectsRelations = relations(
   }),
 );
 
-export const programQualificationsRelations = relations(
-  programQualifications,
-  ({ one }) => ({
-    program: one(programs, {
-      fields: [programQualifications.programId],
-      references: [programs.id],
-    }),
-    qualification: one(qualifications, {
-      fields: [programQualifications.qualificationId],
-      references: [qualifications.id],
-    }),
-  }),
-);
-
-export const subjectsRelations = relations(subjects, ({ one }) => ({
-  qualification: one(qualifications, {
-    fields: [subjects.qualificationId],
-    references: [qualifications.id],
-  }),
-}));
-
 export const documentsRelations = relations(documents, ({ one }) => ({
-  user: one(users, {
-    fields: [documents.userId],
-    references: [users.id],
+  application: one(applications, {
+    fields: [documents.applicationId],
+    references: [applications.id],
   }),
 }));
