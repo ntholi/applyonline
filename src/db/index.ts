8583aa4a71b6b630807ef668eb47e04e6d58f7ee
Ntholi Nkhatho <ntholi@live.com>
2025-03-21 16:54:18 +0200

    init drizzle

---
+++ src/db/index.ts
@@ -0,0 +1,15 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+import * as schema from './schema';
+
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+
+const db = drizzle(client, {
+  schema,
+  casing: 'snake_case',
+});
+
+export { db };
