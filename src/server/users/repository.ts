0682ff79cca47c34fa90a6721ce409e75e621418
Ntholi Nkhatho <ntholi@live.com>
2025-03-22 14:11:27 +0200

    fix: standardize import paths and improve configuration structure in Next.js setup

--- src/server/users/repository.ts
+++ src/server/users/repository.ts
@@ -1,10 +1,7 @@
 import BaseRepository from '@/server/base/BaseRepository';
-import { users } from '@/db/schema'
+import { users } from '@/db/schema/auth';
 
-export default class UserRepository extends BaseRepository<
-  typeof users,
-  'id'
-> {
+export default class UserRepository extends BaseRepository<typeof users, 'id'> {
   constructor() {
     super(users, 'id');
   }
