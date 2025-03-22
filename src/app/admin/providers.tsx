b43ea045e13dcd7c615943d0d8478e7ab004e92d
Ntholi Nkhatho <ntholi@live.com>
2025-03-22 10:14:49 +0200

    feat: add QueryClientProvider to manage React Query state

--- src/app/admin/providers.tsx
+++ src/app/admin/providers.tsx
@@ -4,6 +4,9 @@ import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -11,7 +14,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <Notifications />
       <ModalsProvider>
         <SessionProvider>
-          <NuqsAdapter>{children}</NuqsAdapter>
+          <QueryClientProvider client={queryClient}>
+            <NuqsAdapter>{children}</NuqsAdapter>
+          </QueryClientProvider>
         </SessionProvider>
       </ModalsProvider>
     </MantineProvider>
