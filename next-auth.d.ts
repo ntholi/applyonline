9e5d7b5154fbdabba3b0e154b8ff00f729992da3
Ntholi Nkhatho <ntholi@live.com>
2025-03-22 10:38:16 +0200

    feat: integrate Google provider and extend user model with role in NextAuth

---
+++ next-auth.d.ts
@@ -0,0 +1,8 @@
+import { UserRole } from '@/db/schema';
+import { User as DefaultUser } from 'next-auth';
+
+declare module 'next-auth' {
+  interface User extends DefaultUser {
+    role: UserRole;
+  }
+}
