0334823966818ad4ad05f0aa6ba5752f03cf54e2
Ntholi Nkhatho <ntholi@live.com>
2025-03-24 09:14:35 +0200

    refactor: simplify user details layout and enhance image handling

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,11 +1,7 @@
-import {
-  DetailsView,
-  DetailsViewHeader,
-  FieldView,
-  DetailsViewBody,
-} from '@/components/adease';
+import { DetailsView, DetailsViewHeader, FieldView } from '@/components/adease';
 import { notFound } from 'next/navigation';
 import { getUser, deleteUser } from '@/server/users/actions';
+import { Badge, Grid, GridCol, Group, Image, Stack, Text } from '@mantine/core';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -14,26 +10,53 @@ type Props = {
 export default async function UserDetails({ params }: Props) {
   const { id } = await params;
   const user = await getUser(id);
-  
+
   if (!user) {
     return notFound();
   }
 
   return (
     <DetailsView>
-      <DetailsViewHeader 
-        title={'User'} 
+      <DetailsViewHeader
+        title={'User'}
         queryKey={['users']}
         handleDelete={async () => {
           'use server';
           await deleteUser(id);
         }}
       />
-      <DetailsViewBody>
-        <FieldView label='Name'>{user.name}</FieldView>
-        <FieldView label='Email'>{user.email}</FieldView>
-        <FieldView label='Image'>{user.image}</FieldView>
-      </DetailsViewBody>
+      <Grid p={'lg'}>
+        <GridCol span={6}>
+          <Image
+            src={largeImage(user.image)}
+            w={400}
+            fit='contain'
+            radius={'lg'}
+          />
+        </GridCol>
+        <GridCol span={6}>
+          <Stack gap={'lg'}>
+            <FieldView label='ID'>{user.id}</FieldView>
+            <FieldView label='Name'>{user.name}</FieldView>
+            <FieldView label='Email'>{user.email}</FieldView>
+            <Group>
+              <Text c='dimmed' size='sm'>
+                Role
+              </Text>
+              <Badge variant='outline'>{user.role}</Badge>
+            </Group>
+          </Stack>
+        </GridCol>
+      </Grid>
     </DetailsView>
   );
+}
+
+function largeImage(image: string | null | undefined) {
+  if (!image) return image;
+  const parts = image.split('=');
+  if (parts.length > 1) {
+    return `${parts[0]}=s500-c`;
+  }
+  return image;
 }
